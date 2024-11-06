ca46fa71c8bc5856cd5ecf8d302d678202c7b05f
steehhh <stefanypradebon@gmail.com>
2024-11-06 19:32:16 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+let FinalDeSemana = "sabado";
+
+let SFinalDeSemana = "domingo";
+
+let Resposta = prompt("Que dia é hoje?");
+
+if (Resposta == FinalDeSemana ){
+    alert("Bom fim de semana!")
+} else {
+    alert("Boa semana!")
+}; 
+
